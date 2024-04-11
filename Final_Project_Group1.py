1539fa958ff7e6854f56c993e3fbdbe1e2663223
bjpuckett <156225723+bjpuckett@users.noreply.github.com>
2024-04-11 18:01:42 -0500

    Update Final_Project_Group1.py
    Added basic line to determine player character name.

--- Final_Project_Group1.py
+++ Final_Project_Group1.py
@@ -1,2 +1,10 @@
-Alignment = 0
-PC_Name = 
+alignment = 0
+
+
+def print_menu():
+    print("\nMain Menu")
+    print("1. Start Game")
+    print("2. Exit")
+
+player_character = input("What is your name?")
+print(player_character)
