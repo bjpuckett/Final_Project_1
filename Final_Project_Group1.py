9eac6434924bfa4aa48e6c5972ea652071f6e613
Benjamin Puckett <156225723+bjpuckett@users.noreply.github.com>
2024-04-16 22:33:33 -0500

    Update Final_Project_Group1.py
    Changed a quick typo.

--- Final_Project_Group1.py
+++ Final_Project_Group1.py
@@ -13,7 +13,7 @@ def story_1():
     print("\nWith a deep breath,", player_character, "reaches out to the Troika and makes his choice.\nA warm light envelops his hands as the gift of Celestial healing , and whispers of ancient tongues fill his mind, granting him the powers of Mental Manipulation into his being. ")
     print("\n",player_character,"is teleported to a path that  leads him to a village on the brink of despair.\nThe village almost looks like it is in shambles. The eyes or the villages look like they have lost all sense of purpose and hope. A sickness has taken hold, one that no medicine can cure.\nThe villagers eye him fear, ",player_character,"does look pretty scary, perhaps thinking he is a raider.")
     print("1: Seeing that people are in need and wanting to be just and ease their pain use Celestial Healing  to cure the villagers, potentially revealing his divine gifts to all.")
-    print("2: Use Mind Barrage to delve into the villagers’ minds, seeking the source of their affliction and attempting to free them from its grasp.")
+    print("2: Use Mental Manipulation to delve into the villagers’ minds, seeking the source of their affliction and attempting to free them from its grasp.")
 
 def story_2():
     print("\nStory text")
