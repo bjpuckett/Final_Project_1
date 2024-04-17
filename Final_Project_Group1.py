4334d7724023ae94aa42aa824be55d58737e439d
Benjamin Puckett <156225723+bjpuckett@users.noreply.github.com>
2024-04-16 19:49:36 -0500

    Update Final_Project_Group1.py
    Added 1st part of story.

--- Final_Project_Group1.py
+++ Final_Project_Group1.py
@@ -28,10 +28,11 @@ while(True):
 
 # Defining 1st story function/choice.
 def story_1():
-    print("\nStory text")
-    print("1: 1st option")
-    print("2: 2nd option")
-
+   def story_1(): 
+    print("\nWith a deep breath,", player_character, "reaches out to the Troika and makes his choice.\nA warm light envelops his hands as the gift of Celestial healing , and whispers of ancient tongues fill his mind, granting him the powers of Mental Manipulation into his being. ")
+    print("\n",player_character,"is teleported to a path that  leads him to a village on the brink of despair.\nThe village almost looks like it is in shambles. The eyes or the villages look like they have lost all sense of purpose and hope. A sickness has taken hold, one that no medicine can cure.\nThe villagers eye him fear, ",player_character,"does look pretty scary, perhaps thinking he is a raider.")
+    print("1: Seeing that people are in need and wanting to be just and ease their pain use Celestial Healing  to cure the villagers, potentially revealing his divine gifts to all.")
+    print("2: Use Mind Barrage to delve into the villagers’ minds, seeking the source of their affliction and attempting to free them from its grasp.")
 while(True):
     story_1()
     choice_1 = input("Make your choice: ")
